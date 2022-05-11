e2c9af6fefea388a65272296b00d222e85215859
N <nathanmunn21@gmail.com>
2022-05-11 06:33:25 -0300

    Added Scripts
    Added cow.py and uncow.py to Main Branch.

---
+++ cow.py
@@ -0,0 +1,25 @@
+l_moo = 'moo'
+u_moo = 'MOO'
+
+def cow_main():
+	bit_index = 0
+	while(True):
+		try:
+			line = input();
+			line = line + '\n'
+			for char in line:
+				uni_bin = format(ord(char), '016b')
+				for bit in uni_bin:
+					moo_index = (bit_index % 3)
+					if (bit == '0'):
+						print(l_moo[moo_index], end='')
+					elif (bit == '1'):
+						print(u_moo[moo_index], end='')
+					if (moo_index == 2):
+						print(' ', end='')
+					bit_index += 1
+		except:
+			return;
+
+if (__name__ == '__main__'):
+	cow_main()
---
+++ uncow.py
@@ -0,0 +1,20 @@
+def uncow_main():
+	data = ''
+	while(True):
+		try:
+			line = input()
+			for char in line:
+				if (char in 'mo'):
+					data += '0'
+				if (char in 'MO'):
+					data += '1'
+		except:
+			output = ''
+			for char in range(0,int(len(data)/16)):
+				output += chr(int(data[0:16],2))
+				data = data[16:]
+			print(output, end='')
+			return
+
+if (__name__ == '__main__'):
+	uncow_main()
